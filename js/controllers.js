f91f601471eb61bdbae98e1893e15cf57ebb2975
matiastucci <matiastucci@gmail.com>
2014-11-02 20:54:42 -0300

    Ionic tabs

---
+++ js/controllers.js
@@ -0,0 +1,92 @@
+angular.module('socket-chat.controllers', [])
+
+.controller('ParentCtrl', function($scope, $ionicLoading, $ionicPopup, Chat) {
+
+  $scope.username = Chat.getUsername();
+
+  $scope.showLoading = function(){
+    $ionicLoading.show({
+      noBackdrop: true,
+      template: '<i class="favoriteModal ion-loading-c"></i>'
+    });
+  };
+
+  $scope.hideLoading = function(){
+    $ionicLoading.hide();
+  };
+
+
+  $scope.showUsernamePopup = function() {
+    $scope.data = {}
+
+    var myPopup = $ionicPopup.show({
+      template: '<input type="text" ng-model="data.username" autofocus>',
+      title: 'Choose a username',
+      scope: $scope,
+      buttons: [
+          {
+            text: '<b>Save</b>',
+            type: 'button-positive',
+            onTap: function(e) {
+              if (!$scope.data.username) {
+                //don't allow the user to close unless he enters the username
+                e.preventDefault();
+              } else {
+                return $scope.data.username;
+              }
+            }
+          },
+      ]
+    });
+    myPopup.then(function(res) {
+      Chat.setUsername(res);
+      $scope.username = res;
+    });
+  };
+
+  if(!$scope.username){
+    $scope.showUsernamePopup();
+  }
+
+})
+
+.controller('ChatCtrl', function($scope,$ionicScrollDelegate,Chat) {
+
+  $scope.messages = Chat.getMessages();
+
+  $scope.$watch('newMessage', function(newValue, oldValue) {
+    if(typeof newValue != 'undefined'){
+      if(newValue != ''){
+        Chat.typing();
+      }
+      else{
+        Chat.stopTyping();
+      }
+    }
+  });
+
+  $scope.sendMessage = function() {
+    if($scope.newMessage){
+      Chat.sendMessage($scope.newMessage);
+      $scope.newMessage = '';
+      $ionicScrollDelegate.scrollBottom(true);
+    }
+    else{
+      alert('Can\'t be empty');
+    }
+  };
+
+})
+
+.controller('PeopleCtrl', function($scope, Chat) {
+  $scope.showLoading();
+  Chat.getUsernames().then(function(res){
+    $scope.people  = res.data;
+    $scope.numUsers = Object.keys(res.data).length;
+    $scope.hideLoading();
+  });
+})
+
+.controller('AccountCtrl', function($scope,Chat) {
+  $scope.username = Chat.getUsername();
+});
---
+++ js/services.js
@@ -0,0 +1,99 @@
+angular.module('socket-chat.services', [])
+
+.factory('Chat', function($rootScope,$http,Notification) {
+
+  var username;
+  var messages = [];
+
+  var baseUrl, socket;
+
+  if(window.location.origin.indexOf('localhost') == -1)
+    baseUrl = 'https://socket-chat-server.herokuapp.com:443';
+  else
+    baseUrl = 'http://localhost:8080';
+
+  socket = io.connect(baseUrl);
+
+  var functions = {
+    all: function() {
+      return friends;
+    },
+    get: function(friendId) {
+      return friends[friendId];
+    },
+    getMessages: function(){
+      return messages;
+    },
+    sendMessage: function(msg){
+      messages.push({
+        username: username,
+        content: msg
+      });
+      socket.emit('new message', msg, username);
+    },
+    getUsername: function(){
+      return username;
+    },
+    setUsername: function(newUsername){
+      username = newUsername;
+      socket.emit('add user', username);
+    },
+    getUsernames: function(){
+      return $http.get(baseUrl+'/usernames');
+    },
+    typing: function(){
+      socket.emit('typing');
+    },
+    stopTyping: function(){
+      socket.emit('stop typing');
+    }
+  };
+
+  socket.on('user joined', function(data){
+    Notification.show(data.username + ' connected');
+  });
+
+  socket.on('new message', function(msg){
+    $rootScope.$apply(function () {
+      messages.push(msg);
+    });
+  });
+
+  socket.on('user left', function(data){
+    Notification.show(data.username + ' disconnected');
+  });
+
+  socket.on('typing', function(data){
+    console.log('typing');
+    Notification.show(data.username + ' is typing');
+  });
+
+  socket.on('stop typing', function(data){
+    console.log('stop typing');
+    Notification.hide();
+  });
+
+  return functions;
+
+})
+
+.factory('Notification', function($timeout) {
+
+ return {
+  show: function(msg){
+    var $notificationDiv = angular.element( document.querySelector( '.notification' ) );
+    $notificationDiv.css('display','inherit');
+    $notificationDiv.html(msg);
+    if(msg.indexOf('typing') == -1){
+      $timeout(function(){
+        $notificationDiv.css('display','none');
+      }, 5000);
+    }
+  },
+  hide: function(){
+    var $notificationDiv = angular.element( document.querySelector( '.notification' ) );
+    $notificationDiv.css('display','none');
+  }
+ }
+
+});
