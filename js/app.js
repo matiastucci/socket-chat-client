0576742f29f04e09d315451b99d5fefdb5943d1e
Matias <matiastucci@gmail.com>
2015-07-29 02:27:50 -0500

    removed comments

--- js/app.js
+++ js/app.js
@@ -19,10 +19,6 @@ angular.module('chat', ['ionic', 'chat.controllers', 'chat.services'])
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
 
   // setup an abstract state for the tabs directive
