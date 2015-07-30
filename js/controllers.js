0b3ac18ef65971ca2897d7e1ea6ba4d83f3900f3
Matias <matiastucci@gmail.com>
2015-07-30 02:07:27 -0500

    removed comment

--- js/controllers.js
+++ js/controllers.js
@@ -74,14 +74,6 @@ angular.module('chat.controllers', [])
 })
 
 .controller('PeopleCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
     Chats.remove(chat);
